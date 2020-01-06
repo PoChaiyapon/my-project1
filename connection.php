b6fcd7dbf5dc76bdd2e03c9ed0509b6e3fd758e1
chaiyapon <chaiporn137@gmail.com>
2020-01-06 15:02:15 +0700

    edit config connection

--- connection.php
+++ connection.php
@@ -1,5 +1,31 @@
 <?php
 Class DBConnection{
-    
+    //config setting
+    protected $_config;
+
+    //connection
+    public $dbc;
+
+    //function __construct
+    public function __construct($config){
+        $this->_config = $config;
+    }
+
+    //function __destruct
+    public function __destruct(){
+        $this->dbc = null;
+    }
+
+    //function open getPDOConnection();
+    private function getPDOConnection(){
+        if($this->dbc != null){
+            try{
+
+            }
+            catch(PDOException $e){
+                echo __LINE__.$e->getMessage();
+            }
+        }
+    }
 }
 ?>
